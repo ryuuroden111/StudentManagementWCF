8f2370815185095f3346bd8d6c6da5bda819d8fc
Nguyen Thai Sang <ryuuroden111@gmail.com>
2016-12-15 23:13:31 +0700

    them attributes cho BDO

--- QuanLySinhVienBDO/MonHocBDO.cs
+++ QuanLySinhVienBDO/MonHocBDO.cs
@@ -3,15 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Runtime.Serialization;
 
 namespace QuanLySinhVienWCF
 {
     namespace QuanLySinhVienBDO
     {
+        [DataContract]
         public class MonHocBDO
         {
+            [DataMember]
             public string MaMH { get; set; }
+            [DataMember]
             public string TenMH { get; set; }
+            [DataMember]
             public int SoTiet { get; set; }
         }
     }
